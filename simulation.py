2003f0bdf22e195822609fedd25be0e6b7e95b0a
Michal Martínek <michal7952@seznam.cz>
2024-03-05 21:26:44 +0100

    Force visualization

--- simulation.py
+++ simulation.py
@@ -1,12 +1,15 @@
 import numpy as np
+import pygame
 
 NUM_PLANES = 1
 GRAVITY = 5.
 ENGINE_ACCEL = 3.
-LIFT_COEFF = 0.1
-LIFT_LEVER_COEFF = 0.001
+LIFT_COEFF = 0.2
+LIFT_LEVER_COEFF = 0.0001
 DRAG_COEFF = 0.1
 ANGULAR_DRAG_COEFF = 0.03
+ELEVATOR_MAX_ANGLE = 0.4 # ~ 23 degrees
+ELEVATOR_LEVER_COEFF = 0.2
 
 class Simulation:
 	def __init__(self):
@@ -15,7 +18,7 @@ class Simulation:
 		self.angles = np.zeros(NUM_PLANES)
 		self.angularVels = np.zeros(NUM_PLANES)
 
-	def update(self, timedelta, controlInputs):
+	def update(self, timedelta, controlInputs, *, drawVectors=True):
 		self.positions += self.speeds * timedelta
 		self.speeds[:, 1] += GRAVITY * timedelta
 		self.speeds += timedelta * (enginePower := self.rotate((ENGINE_ACCEL, 0)) * controlInputs[0])
@@ -28,6 +31,14 @@ class Simulation:
 		self.angles += self.angularVels * timedelta
 		self.angularVels -= timedelta * LIFT_LEVER_COEFF * self.airflowSpeed() ** 2
 		self.angularVels -= ANGULAR_DRAG_COEFF * timedelta * self.angularVels * np.abs(self.angularVels)
+
+		if drawVectors:
+			self.drawVec(6 * self.speeds)
+			self.drawVec(15 * enginePower, (0, 255, 0))
+			self.drawVec(6 * self.rotate((self.airflowSpeed()[0], 0)), (255, 255, 255))
+			self.drawVec(15 * liftVec, (0, 255, 255))
+			self.drawVec(-6 * dragVec, (0, 0, 255))
+
 	def airflowSpeed(self):
 		'''speed of airflow around wings'''
 		coords = self.rotate((1, 0)) * self.speeds
@@ -37,3 +48,8 @@ class Simulation:
 		rotMat = np.moveaxis(np.array( ((np.cos(self.angles), np.sin(self.angles)), (-np.sin(self.angles), np.cos(self.angles))) ), -1, 0)
 		product = rotMat * vecs[:, np.newaxis]
 		return np.sum(product, -1)
+
+	def drawVec(self, vec, color=(255, 0, 0)):
+		while len(vec.shape) > 1: vec = vec[0]
+		line = self.positions[0], (self.positions[0] + vec)
+		pygame.draw.line(pygame.display.get_surface(), color, *line)
